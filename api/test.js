1f92d65410086ad04da5a09cea6344fd329abc62
SpeeedWagon <gabriel.duda@deepcode.xyz>
2021-07-20 21:29:43 +0300

    656

--- api/test.js
+++
@@ -1,9 +0,0 @@
-module.exports = async (req, res) => {
-  res.json({
-    url: req.url,
-    originalUrl: req.originalUrl,
-    body: req.body,
-    query: req.query,
-    cookies: req.cookies,
-  });
-};
