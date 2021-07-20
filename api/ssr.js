ebbdd7675e5d0fb6b6e95279e615781f2cc47dbd
SpeeedWagon <gabriel.duda@deepcode.xyz>
2021-07-20 14:52:13 +0300

    223

---
+++ api/ssr.js
@@ -0,0 +1,12 @@
+require("../dist/server/importer.js");
+const { createPageRender } = require("vite-plugin-ssr");
+
+const renderPage = createPageRender({ isProduction: true });
+
+module.exports = async (req, res) => {
+  const url = req.originalUrl || req.url;
+  const pageContext = { url };
+  const result = await renderPage(pageContext);
+  if (result.nothingRendered) return;
+  res.status(result.statusCode).send(result.renderResult);
+};
